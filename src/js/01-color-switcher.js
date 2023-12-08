3882d8e4014e6269e7713741b9ab6e7fdef4d57e
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2023-12-08 02:31:48 +0100

    hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,188 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
+
+refs.startButton.addEventListener('click', toTargetRun);
+refs.stopButton.addEventListener('click', toTargetStop);
+
+let timerId = null;
+
+// ====================
+let element = 0;
+// ====================
+
+function toTargetRun() {
+  // Забороняємо натискання кнопки "Start"
+  refs.startButton.disabled = true;
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    console.log(`random color ${++element}:  ${getRandomHexColor()}`);
+  }, 1000);
+}
+function toTargetStop() {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+  // Дозволяємо натискання кнопки "Start" після зупинки
+  refs.startButton.disabled = false;
+}
+
+//
+///
+
+///
+
+// const preciseTeamMeetingDate = new Date('March 16, 2030 14:25:00');
+// console.log(preciseTeamMeetingDate.setMinutes(50));
+
+// const date = new Date();
+// console.log(
+//   'Today is: ',
+//   date.getDate(),
+//   '-',
+//   date.getMonth(),
+//   '-',
+//   date.getFullYear(),
+//   ' ----  ',
+//   date.setMinutes(5)
+// );
+
+// const date = new Date('March 16, 2030 14:25:00');
+
+// console.log(date.setMinutes(50), date.setFullYear(2040, 4, 8));
+
+// const date = new Date('March 16, 2030 14:25:00');
+
+// console.log(
+//   date.toString() +
+//     '\n' +
+//     date.toTimeString() +
+//     '\n' +
+//     date.toLocaleTimeString() +
+//     '\n' +
+//     date.toUTCString() +
+//     '\n' +
+//     date.toDateString() +
+//     '\n' +
+//     date.toISOString() +
+//     '\n' +
+//     date.toLocaleString() +
+//     '\n' +
+//     date.getTime()
+// );
+
+// // Change value of isSuccess variable to call resolve or reject
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve('Success! Value passed to resolve function');
+//     } else {
+//       reject('Error! Error passed to reject function');
+//     }
+//   }, 2000);
+//   console.log(resolve, reject);
+// });
+///////////////////////////////////////////////////////
+// Функція, яка повертає Promise і виконується асинхронно
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     // Симулюємо асинхронний запит даних
+//     setTimeout(() => {
+//       const data = 'Дані успішно завантажено';
+
+//       // Перевірка, чи дані були успішно завантажені чи ні
+//       if (data) {
+//         resolve(data); // Викликаємо resolve у випадку успішного завершення
+//       } else {
+//         reject('Не вдалося завантажити дані'); // Викликаємо reject у випадку помилки
+//       }
+//     }, 2000); // Симуляція 2-секундного запиту
+//   });
+// }
+
+// // Викликаємо функцію з Promise
+// fetchData()
+//   .then(result => {
+//     console.log('Успіх:', result);
+//   })
+//   .catch(error => {
+//     console.error('Помилка:', error);
+//   });
+/////////////////////////////////////////////////////////////////////////
+
+// function fetchData(shouldSucceed) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (shouldSucceed) {
+//         resolve('Дані успішно завантажено');
+//       } else {
+//         reject('Не вдалося завантажити дані');
+//       }
+//     }, 100);
+//   });
+// }
+
+// // Викликати функцію з передачею true, якщо ви хочете, щоб Promise був вирішений успішно
+// fetchData(true)
+//   .then(result => {
+//     console.log('Успіх:', result);
+//   })
+//   .catch(error => {
+//     console.error('Помилка:', error);
+//   });
+
+//////////////////////////////////////////////////////////
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Гра вгадай слово</title>
+//   <script src="game.js" defer></script>
+// </head>
+// <body>
+
+// <h1>Гра вгадай слово</h1>
+// <p id="result"></p>
+// <input type="text" id="guessInput" placeholder="Введіть слово">
+// <button onclick="checkGuess()">Підтвердити</button>
+
+// </body>
+// </html>
+// *****************************
+// game.js
+
+// function fetchData(shouldSucceed) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (shouldSucceed) {
+//         resolve('Ви вгадали слово!');
+//       } else {
+//         reject('Невірно, ви програли.');
+//       }
+//     }, 2000);
+//   });
+// }
+
+// function checkGuess() {
+//   const guessInput = document.getElementById('guessInput').value.toLowerCase();
+
+//   // Викликаємо асинхронну задачу, яка повертає Promise
+//   fetchData(guessInput === 'Каденюк')
+//     .then(result => {
+//       document.getElementById('result').innerText = result;
+//     })
+//     .catch(error => {
+//       document.getElementById('result').innerText = error;
+//     });
+// }
+//////////////////////////////////////////////
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,90 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delayInput: document.querySelector('[name="delay"]'),
+  stepInput: document.querySelector('[name="step"]'),
+  amountInput: document.querySelector('[name="amount"]'),
+  button: document.querySelector('button'),
+};
+
+refs.form.addEventListener('submit', toTargetSubmit);
+refs.delayInput.addEventListener('input', toTargetInput);
+refs.stepInput.addEventListener('input', toTargetInput);
+refs.amountInput.addEventListener('input', toTargetInput);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function toTargetSubmit(evt) {
+  evt.preventDefault();
+  const form = evt.target;
+  let delayInput = Number(refs.delayInput.value);
+  let stepInput = Number(refs.stepInput.value);
+  let amountInput = Number(refs.amountInput.value);
+
+  for (let i = 1; i <= amountInput; i++) {
+    const existingNotification = document.querySelector(
+      '.notiflix-notification'
+    );
+    if (existingNotification) {
+      existingNotification.style.display = 'none';
+    }
+
+    createPromise(i, delayInput)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayInput += stepInput;
+  }
+}
+// //////////////////////////////////////
+function toTargetInput() {
+  const delayInputValue = refs.delayInput.value;
+  const stepInputValue = refs.stepInput.value;
+  const amountInputValue = refs.amountInput.value;
+
+  if (
+    delayInputValue !== '' &&
+    stepInputValue !== '' &&
+    amountInputValue !== ''
+  ) {
+    const delayValuePromise = new Promise(resolve => {
+      resolve(Number(delayInputValue));
+    });
+
+    const stepValuePromise = new Promise(resolve => {
+      resolve(Number(stepInputValue));
+    });
+
+    const amountValuePromise = new Promise(resolve => {
+      resolve(Number(amountInputValue));
+    });
+
+    Promise.all([delayValuePromise, stepValuePromise, amountValuePromise])
+      .then(([delayValue, stepValue, amountValue]) => {
+        const message = `Delay: ${delayValue}, Step: ${stepValue}, Amount: ${amountValue}`;
+        Notiflix.Notify.info(message);
+      })
+      .catch(error => {
+        Notiflix.Notify.failure(`Error: ${error}`);
+      });
   }
 }
