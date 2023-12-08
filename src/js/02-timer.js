09658153d6f6d7218bbc60baef5e51038ae30ee7
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2023-12-08 16:19:18 +0100

    hw-1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,7 +12,6 @@ refs.startBtn.disabled = true;
 
 let intervalId = null;
 let selectedDate;
-let now;
 
 const options = {
   enableTime: true,
@@ -22,10 +21,10 @@ const options = {
   onClose(selectedDates) {
     selectedDate = selectedDates[0];
     //
-    setTimerFromInput();
+    createTimer();
     //
     console.log(selectedDates[0]);
-    now = new Date().getTime();
+    const now = new Date().getTime();
     if (selectedDate < now) {
       refs.startBtn.disabled = true;
       Notiflix.Notify.failure('Please choose a date in the future');
@@ -38,14 +37,23 @@ flatpickr(refs.input, options);
 
 refs.startBtn.addEventListener('click', toBtnClick);
 
-//////////////////////////////////
-// Функція для встановлення часу на таймері з вибраної дати
-function setTimerFromInput() {
+/////////////////////////////////////////////////////////////////////////////////
+
+// Обробник кліку на кнопці "Start"
+function toBtnClick() {
+  refs.startBtn.disabled = true;
+  refs.input.disabled = true;
+  clearInterval(intervalId);
+  intervalId = setInterval(createTimer, 1000);
+}
+
+function createTimer() {
   const currentTime = new Date().getTime();
   const selectedDateTime = selectedDate.getTime();
 
   if (selectedDateTime < currentTime) {
-    Notiflix.Notify.failure('Please choose a future date');
+    Notiflix.Notify.failure('Please choose a date in the future');
+    clearInterval(intervalId);
     return;
   }
 
@@ -54,35 +62,13 @@ function setTimerFromInput() {
   refs.timerID[1].textContent = addLeadingZero(timer.hours);
   refs.timerID[2].textContent = addLeadingZero(timer.minutes);
   refs.timerID[3].textContent = addLeadingZero(timer.seconds);
+
+  if (selectedDate - currentTime <= 1000) {
+    clearInterval(intervalId);
+    Notiflix.Notify.success('Please choose a date in the future');
+  }
 }
-
-// .////////////\///////////
-// Обробник кліку на кнопці "Start"
-function toBtnClick() {
-  refs.startBtn.disabled = true;
-  refs.input.disabled = true;
-  clearInterval(intervalId);
-
-  intervalId = setInterval(() => {
-    const currentTime = new Date().getTime();
-
-    if (selectedDate - currentTime <= 0) {
-      clearInterval(intervalId);
-      Notiflix.Notify.success('Please choose a date in the future');
-    }
-
-    const timer = convertMs(selectedDate - currentTime);
-    refs.timerID[0].textContent = addLeadingZero(timer.days);
-    refs.timerID[1].textContent = addLeadingZero(timer.hours);
-    refs.timerID[2].textContent = addLeadingZero(timer.minutes);
-    refs.timerID[3].textContent = addLeadingZero(timer.seconds);
-
-    // Object.entries(timer).forEach(([, value], index) => {
-    //   refs.timerID[index].textContent = addLeadingZero(value);
-    // });
-  }, 1000);
-}
-
+// /////////////////////////////////////////////////////////////////////////////
 // Функція для додавання ведучого нуля до числа
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
@@ -101,3 +87,117 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
+
+//
+//
+//
+//
+// ////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////
+//
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+// import Notiflix from 'notiflix';
+
+// const refs = {
+//   timerID: document.querySelectorAll('.value'),
+//   input: document.querySelector('input#datetime-picker'),
+//   startBtn: document.querySelector('button[data-start]'),
+// };
+
+// refs.startBtn.disabled = true;
+
+// let intervalId = null;
+// let selectedDate;
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     selectedDate = selectedDates[0];
+//     //
+//     setTimerFromInput();
+//     //
+//     console.log(selectedDates[0]);
+//     const now = new Date().getTime();
+//     if (selectedDate < now) {
+//       refs.startBtn.disabled = true;
+//       Notiflix.Notify.failure('Please choose a date in the future');
+//     } else {
+//       refs.startBtn.disabled = false;
+//     }
+//   },
+// };
+// flatpickr(refs.input, options);
+
+// refs.startBtn.addEventListener('click', toBtnClick);
+
+// //////////////////////////////////
+// // Функція для встановлення часу на таймері з вибраної дати
+// function setTimerFromInput() {
+//   const currentTime = new Date().getTime();
+//   const selectedDateTime = selectedDate.getTime();
+
+//   if (selectedDateTime < currentTime) {
+//     Notiflix.Notify.failure('Please choose a future date');
+//     return;
+//   }
+
+//   const timer = convertMs(selectedDateTime - currentTime);
+//   refs.timerID[0].textContent = addLeadingZero(timer.days);
+//   refs.timerID[1].textContent = addLeadingZero(timer.hours);
+//   refs.timerID[2].textContent = addLeadingZero(timer.minutes);
+//   refs.timerID[3].textContent = addLeadingZero(timer.seconds);
+// }
+
+// // .////////////\///////////
+// // Обробник кліку на кнопці "Start"
+// function toBtnClick() {
+//   refs.startBtn.disabled = true;
+//   refs.input.disabled = true;
+//   clearInterval(intervalId);
+
+//   intervalId = setInterval(() => {
+//     const currentTime = new Date().getTime();
+
+//     if (selectedDate - currentTime <= 0) {
+//       clearInterval(intervalId);
+//       Notiflix.Notify.success('Please choose a date in the future');
+//     }
+
+//     const timer = convertMs(selectedDate - currentTime);
+//     refs.timerID[0].textContent = addLeadingZero(timer.days);
+//     refs.timerID[1].textContent = addLeadingZero(timer.hours);
+//     refs.timerID[2].textContent = addLeadingZero(timer.minutes);
+//     refs.timerID[3].textContent = addLeadingZero(timer.seconds);
+
+//     // Object.entries(timer).forEach(([, value], index) => {
+//     //   refs.timerID[index].textContent = addLeadingZero(value);
+//     // });
+//   }, 1000);
+// }
+
+// // Функція для додавання ведучого нуля до числа
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+// // Функція для конвертації мілісекунд в об'єкт {days, hours, minutes, seconds}
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   return { days, hours, minutes, seconds };
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,16 +2,9 @@ import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector('.form'),
-  delayInput: document.querySelector('[name="delay"]'),
-  stepInput: document.querySelector('[name="step"]'),
-  amountInput: document.querySelector('[name="amount"]'),
-  button: document.querySelector('button'),
 };
 
 refs.form.addEventListener('submit', toTargetSubmit);
-refs.delayInput.addEventListener('input', toTargetInput);
-refs.stepInput.addEventListener('input', toTargetInput);
-refs.amountInput.addEventListener('input', toTargetInput);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -28,19 +21,12 @@ function createPromise(position, delay) {
 
 function toTargetSubmit(evt) {
   evt.preventDefault();
-  const form = evt.target;
-  let delayInput = Number(refs.delayInput.value);
-  let stepInput = Number(refs.stepInput.value);
-  let amountInput = Number(refs.amountInput.value);
+
+  let delayInput = Number(evt.target.elements.delay.value);
+  let stepInput = Number(evt.target.elements.step.value);
+  let amountInput = Number(evt.target.elements.amount.value);
 
   for (let i = 1; i <= amountInput; i++) {
-    const existingNotification = document.querySelector(
-      '.notiflix-notification'
-    );
-    if (existingNotification) {
-      existingNotification.style.display = 'none';
-    }
-
     createPromise(i, delayInput)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
@@ -53,38 +39,5 @@ function toTargetSubmit(evt) {
         );
       });
     delayInput += stepInput;
-  }
-}
-// //////////////////////////////////////
-function toTargetInput() {
-  const delayInputValue = refs.delayInput.value;
-  const stepInputValue = refs.stepInput.value;
-  const amountInputValue = refs.amountInput.value;
-
-  if (
-    delayInputValue !== '' &&
-    stepInputValue !== '' &&
-    amountInputValue !== ''
-  ) {
-    const delayValuePromise = new Promise(resolve => {
-      resolve(Number(delayInputValue));
-    });
-
-    const stepValuePromise = new Promise(resolve => {
-      resolve(Number(stepInputValue));
-    });
-
-    const amountValuePromise = new Promise(resolve => {
-      resolve(Number(amountInputValue));
-    });
-
-    Promise.all([delayValuePromise, stepValuePromise, amountValuePromise])
-      .then(([delayValue, stepValue, amountValue]) => {
-        const message = `Delay: ${delayValue}, Step: ${stepValue}, Amount: ${amountValue}`;
-        Notiflix.Notify.info(message);
-      })
-      .catch(error => {
-        Notiflix.Notify.failure(`Error: ${error}`);
-      });
   }
 }
